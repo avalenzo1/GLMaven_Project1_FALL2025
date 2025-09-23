75aabc7981952497b9a33519e36f9255144390d6
Andrew Mcclendon <ajaj48412@outlook.com>
2025-09-23 09:34:04 -0500

    Added info to GlProject to implement the list

--- src/main/java/GLProject.java
+++ src/main/java/GLProject.java
@@ -1,11 +1,30 @@
 
 //Andrew Mcclendon 668607463 amccl22@uic.edu
 //Anthony Valenzo  667367644 avale3@uic.edu
+
+import java.util.Iterator;
+
 public class GLProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to project 1");
-		
-	}
+        GenericList list = new GenericList() {
+            @Override
+            public void add(Object data) {
+
+            }
+
+            @Override
+            public Object delete() {
+                return null;
+            }
+
+            @Override
+            public Iterator iterator() {
+                return null;
+            }
+        }
+
+    }
 }
