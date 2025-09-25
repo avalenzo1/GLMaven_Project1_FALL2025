3789f599372a0f968809ba14f0281fd216307d48
Andrew Mcclendon <ajaj48412@outlook.com>
2025-09-25 10:19:24 -0500

    added add and delete to GenericQueue

--- src/main/java/GenericQueue.java
+++ src/main/java/GenericQueue.java
@@ -1,5 +1,38 @@
-public abstract class GenericQueue<T> extends GenericList<T> {
+import java.util.Iterator;
+
+public class GenericQueue<T> extends GenericList<T> {
      Node<T> tail;
+
+    @Override
+    public void add(T data) {
+        Node<T> newNode = new Node<T>();
+        newNode.data = data;
+        if(getLength()==0){
+            setHead(newNode);
+        }
+        else{
+            tail.next = newNode;
+        }
+    }
+
+    @Override
+    public T delete() {
+        Node<T> tempHead = new Node<T>();
+
+        if(getLength() == 0) {
+            return null;
+        }
+        else{
+           tempHead = getHead();
+           setHead(tempHead.next);
+        }
+        return tempHead.data;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
 
 
 }
