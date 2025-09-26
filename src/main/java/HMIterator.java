5542c8ad1f4fc8ed95e1ab2aa87943e4e8e1307d
Anthony Valenzo <94491552+avalenzo1@users.noreply.github.com>
2025-09-26 17:25:26 -0500

    added HashMap class, iterator + basic boiler plate functions.

---
+++ src/main/java/HMIterator.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public class HMIterator<T> implements Iterator<T> {
+    HMIterator(MyHashMap<T> myHashMap) {
+
+    }
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+}
--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -1,9 +1,52 @@
-public class MyHashMap<T> extends GenericQueue<T> {
-    MyHashMap(String key, T Value) {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class MyHashMap<T> implements Iterable<T> {
+    ArrayList<GenericQueue<T>> map;
+    int size;
+
+    MyHashMap(String key, T value) {
+        map = new ArrayList<>();
+        size = 10;
+
+        put(key, value);
     }
 
-    void put(String key, T Value) {
-
+    void put(String key, T value) {
+        key.hashCode();
     }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new HMIterator<>(this);
+    }
+
+    // this method will check to see if the given key exists in
+    // the HashMap and return true if yes and false if no.
+    public boolean contains(String key) {
+        return false;
+    }
+
+    // this method will return the value at the given key or return null if
+    // it does not exist.
+    public T get(String key) {
+        return null;
+    }
+
+    // returns the number of key-value mappings in the map.
+    public int size() {
+        return 0;
+    }
+
+    //  returns true if this map contains no key-value mappings.
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // replaces the entry for the specified key only if it is
+    // currently mapped to some value.
+    public T replace(String key, T value) {
+        return null;
+    }
+
 }
